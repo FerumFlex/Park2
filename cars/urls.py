18328dd787859da152283b9b0b79a29d1e86cb44
FireSwami <86517715+FireSwami@users.noreply.github.com>
2021-12-02 16:22:30 +0200

    11

--- cars/urls.py
+++ cars/urls.py
@@ -1,11 +1,18 @@
 from rest_framework import routers
 from .views import DriverViewSet, VehicleViewSet, VehicleDriverView
-
+from django.urls import path
+from cars import views
 
 router = routers.DefaultRouter()
 router.register('drivers/driver', DriverViewSet, 'driver')
 router.register('vehicles/vehicle', VehicleViewSet, 'vehicle')
-router.register('vehicles/vehicle/driver', VehicleDriverView, 'vehicle')
+# router.register('vehicles/vehicle/driver', VehicleDriverView, 'vehicle')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('drivers/driver/<int:pk>/', views.VehicleDriverView.as_view()),
+]
+
+
+urlpatterns += router.urls
 
