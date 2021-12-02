90909ba920329d7347a7348f8c479951a2d34029
FireSwami <86517715+FireSwami@users.noreply.github.com>
2021-12-02 15:53:19 +0200

    9

--- cars/urls.py
+++ cars/urls.py
@@ -5,7 +5,7 @@ from .views import DriverViewSet, VehicleViewSet, VehicleDriverView
 router = routers.DefaultRouter()
 router.register('drivers/driver', DriverViewSet, 'driver')
 router.register('vehicles/vehicle', VehicleViewSet, 'vehicle')
-# router.register('vehicles/vehicle/driver', VehicleDriverView, 'vehicle')
+router.register('vehicles/vehicle/driver', VehicleDriverView, 'vehicle')
 
 urlpatterns = router.urls
 
