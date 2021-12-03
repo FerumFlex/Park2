5cef309e6d39a61ec35c35793dff6c7babb4c9e5
fireswami <jobforsoul@gmail.com>
2021-12-04 00:37:00 +0200

    16

---
+++ cars/tests/test_vehicle.py
@@ -0,0 +1,78 @@
+import json
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import RequestsClient
+
+from cars.models import Driver, Vehicle
+from cars.serializers import DriverSerializer, VehicleSerializer
+
+
+class DriverApiTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test_username')
+        self.driver_1 = Driver.objects.create(first_name="fname1", last_name="lname1")
+        self.driver_2 = Driver.objects.create(first_name="fname2", last_name="lname2")
+        self.driver_3 = Driver.objects.create(first_name="fname3", last_name="lname3")
+
+    def test_count(self):
+        self.assertEqual(3, Driver.objects.all().count())
+
+    def test_get_driver(self):
+        response = self.client.get(reverse('driver-list'))
+        serializer_data = DriverSerializer([self.driver_1, self.driver_2, self.driver_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        data = {
+            "first_name": "TestFirstname",
+            "last_name": "TestLastname"
+        }
+        response = self.client.post('/drivers/driver/', data=data, format="json")
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Driver.objects.all().count())
+        self.assertEqual(data["last_name"], Driver.objects.all()[0].last_name)
+        self.assertEqual(data["first_name"], Driver.objects.all()[0].first_name)
+
+    def test_update(self):
+        data = {
+            "first_name": "Firstname",
+            "last_name": "Lastname"
+        }
+        self.client.force_login(self.user)
+        response = self.client.put('/drivers/driver/3/', data=data, format="json")
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.driver_3.refresh_from_db()
+        self.assertEqual(data["first_name"], self.driver_3.first_name)
+        self.assertEqual(data["last_name"], self.driver_3.last_name)
+
+    def test_delete(self):
+        self.client.force_login(self.user)
+        response = self.client.delete(reverse('driver-detail', kwargs={'pk': 2}))
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(2, Driver.objects.all().count())
+
+    def test_filter(self):
+        response = self.client.get('/drivers/driver/', follow=True,
+                                   data={"created_at": 'created_at__gte'})
+        serializer_data = DriverSerializer([self.driver_1, self.driver_2, self.driver_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_fail_driver_detail(self):
+        response = self.client.get(reverse('driver-detail', kwargs={'pk': 10}))
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+    def test_driver_detail(self):
+        response = self.client.get(reverse('driver-detail', kwargs={'pk': self.driver_3.id}))
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(response.json().get('first_name'), "fname3")
+        self.assertEqual(response.json().get('last_name'), "lname3")
+
+    def test_driver_list(self):
+        response = self.client.get(reverse('driver-list'))
+        # serializer_data = DriverSerializer([self.driver_1, self.driver_2, self.driver_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertTrue({'id': 1, 'first_name': 'fname1', 'last_name': 'lname1'} in response.json())
